e3f2e82860a09f6b920618ff23222b794de46c17
Akshat123120 <74185066+Akshat123120@users.noreply.github.com>
2022-07-13 17:36:16 +0530

    Add files via upload

--- Project 131/laptop.js
+++ Project 131/laptop.js
@@ -1,12 +1,30 @@
+status = "";
 function preload(){
    img = loadImage('61s7sJEpsVL._SX425_.jpg')
 }
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status:Detecting Objects"
 }
 function draw(){
     image(img,0,0,640,420)
     fill("#FF0000");
     noFill();
+}
+function back(){
+    window.location = "index.html";
+}
+function modelLoaded(){
+    status = true;
+    objectDetector.detect(img,gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
 }
--- Project 131/tv.js
+++ Project 131/tv.js
@@ -1,12 +1,30 @@
+status = "";
 function preload(){
-    img = loadImage('tv.jpg')
- }
- function setup(){
-     canvas = createCanvas(640,420);
-     canvas.center();
- }
- function draw(){
-     image(img,0,0,640,420)
-     fill("#FF0000");
-     noFill();
- }
+   img = loadImage('tv.jpg')
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status:Detecting Objects"
+}
+function draw(){
+    image(img,0,0,640,420)
+    fill("#FF0000");
+    noFill();
+}
+function back(){
+    window.location = "index.html";
+}
+function modelLoaded(){
+    status = true;
+    objectDetector.detect(img,gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
